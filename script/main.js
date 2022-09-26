738bd75871d29acdf7576d63495d1c534735e778
tnalss <indoubt@naver.com>
2022-09-26 22:06:11 +0900

    burger animation

--- script/main.js
+++ script/main.js
@@ -1,34 +1,35 @@
 window.addEventListener("DOMContentLoaded", function() {
-	
+
 	const PREV = document.querySelector(".prev");
 	const NEXT = document.querySelector(".next");
 	const PROJECTS = document.querySelector("#projects");
 	const BURGER = document.querySelector(".burger");
-	
-	
-	let postIndex=1;
-	
 
-	NEXT.addEventListener("click", function(){
+
+	let postIndex = 1;
+
+
+	NEXT.addEventListener("click", function() {
 		postIndex++;
-		if(postIndex>6) postIndex=1;		
-		let coords = -880 * (postIndex-1) + "px";
-		PROJECTS.style.marginLeft=coords;
-		
+		if (postIndex > 6) postIndex = 1;
+		let coords = -880 * (postIndex - 1) + "px";
+		PROJECTS.style.marginLeft = coords;
+
 	})
 	//>= 
-	PREV.addEventListener("click", function(){
+	PREV.addEventListener("click", function() {
 		postIndex--;
-		if(postIndex<=0) postIndex=6;		
-		let coords = -880 * (postIndex-1) + "px";
-		PROJECTS.style.marginLeft=coords;
-		
-//postIndex = 
-				
+		if (postIndex <= 0) postIndex = 6;
+		let coords = -880 * (postIndex - 1) + "px";
+		PROJECTS.style.marginLeft = coords;
+
+		//postIndex = 
+
 	})
-	
+
 	BURGER.addEventListener("click", function(){
-		const menu= document.querySelector("#sidePane");
+		BURGER.classList.toggle('on');
+		const menu= document.querySelector("#sidePanel");
 		menu.classList.toggle('d-none');
 	})
 
